3bbb75e4e906754e1618ea852dd0d78ee6ff395e
Islam Mahrous <isl.mahrous@gmail.com>
2022-03-19 17:57:21 +0200

    Add project files.

---
+++ El3edda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace El3edda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
