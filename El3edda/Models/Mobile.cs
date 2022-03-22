d4589a3c1738410fca5430ab39047f6d706fca15
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-03-22 11:40:15 +0200

    s

--- El3edda/Models/Mobile.cs
+++ El3edda/Models/Mobile.cs
@@ -1,19 +1,24 @@
 ﻿using El3edda.Data.Base;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace El3edda.Models
 {
+    [Index(nameof(Serial), IsUnique = true)]
     public class Mobile : IEntityBase
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        public int Serial { get; set; }
         [Required]
         [StringLength(maximumLength:50, ErrorMessage = "Phone name cannot be more than 50 characters.")]
         public string Name { get; set; }
         [Display(Name ="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        [Required]
-        public Manufacturer Manufacturer { get; set; }
+
         [Required]
         public double Price { get; set; }
         [StringLength(maximumLength: 200, ErrorMessage = "Description is too long.")]
@@ -32,7 +37,8 @@ namespace El3edda.Models
         public Specs Specs { get; set; }
         [Required]
         public ICollection<Media> Media { get; set; }
-
+        [Required]
+        public virtual Manufacturer Manufacturer { get; set; }
 
     }
 }
