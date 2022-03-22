cfe2848e0a24fc3158a26bc5dd924fd03811ef53
AhmSaleh <ahmedsaleh166@gmail.com>
2022-03-22 10:22:23 +0200

    Manufacturer Service added with ManufacturersController

--- El3edda/Data/AppDbContext.cs
+++ El3edda/Data/AppDbContext.cs
@@ -3,18 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace El3edda.Data
 {
-
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         //Many to Many --> OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Mobile>().OwnsMany(mob => mob.Media,
-                media =>  media.WithOwner().HasForeignKey("MobileId")
-                );
+            modelBuilder
+                .Entity<Mobile>()
+                .OwnsMany(mob => mob.Media, media => media.WithOwner().HasForeignKey("MobileId"));
             base.OnModelCreating(modelBuilder);
         }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
     }
 }
