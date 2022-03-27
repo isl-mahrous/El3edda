9132c19637578b66a94a4e6e3e40b8d463859f65
abdo hashem <ar1813@fayoum.edu.eg>
2022-03-27 21:32:47 +0200

    add manfucrere filter

---
+++ El3edda/Views/Mobiles/Components/FilterMobile/FilterViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using El3edda.Data;
+using El3edda.Models;
+using El3edda.Data.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace El3edda.Views.Mobiles.Components.FilterMobile
+{
+    public class FilterMobileViewComponent : ViewComponent
+    {
+
+
+        public FilterMobileViewComponent(AppDbContext _context, IManufacturerService manufacturerService)
+        {
+            Context = _context;
+            ManufacturerService = manufacturerService;
+        }
+
+        public AppDbContext Context { get; }
+        public IManufacturerService ManufacturerService { get; }
+
+        public async Task<IViewComponentResult> InvokeAsync(specSearchParamter searchParamter)
+        {
+            //var items = await GetItemsAsync(maxPriority, isDone);
+
+            var manufacturers_list = new SelectList(
+                await ManufacturerService.GetAllAsync(),
+                "Id", "Name");
+
+            if (searchParamter != null)
+            {
+                foreach (var manfcture in manufacturers_list)
+                {
+                    if (searchParamter.manufacturers.Any(m => m.Id == int.Parse(manfcture.Value)))
+                    {
+                        manfcture.Selected = true;
+                    }
+                }
+            }
+            ViewBag.Manufacturers = manufacturers_list;
+
+
+            return View(searchParamter);
+        }
+
+    }
+}
