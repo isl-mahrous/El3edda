0baf0e085a7bd1e41d337646b0b415c6bad01da0
AhmSaleh <ahmedsaleh166@gmail.com>
2022-04-04 01:50:20 +0200

    header and footer links implemented

--- El3edda/Data/ViewComponents/ShoppingCartSummary.cs
+++ El3edda/Data/ViewComponents/ShoppingCartSummary.cs
@@ -3,9 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace El3edda.Data.ViewComponents
 {
-    public class ShoppingCartSummary:ViewComponent
+    public class ShoppingCartSummary : ViewComponent
     {
         private readonly ShoppingCart _shoppingCart;
+
         public ShoppingCartSummary(ShoppingCart shoppingCart)
         {
             _shoppingCart = shoppingCart;
