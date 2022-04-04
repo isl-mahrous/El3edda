93c797d2236cade275253f84f2f40cc39ef75c15
mohamed-fathy96 <pet.mohamedfathy@gmail.com>
2022-04-04 18:03:45 +0200

    landing page work

--- El3edda/Controllers/HomeController.cs
+++ El3edda/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using El3edda.Models;
+﻿using El3edda.Data.Services.MobileService;
+using El3edda.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,14 +9,20 @@ namespace El3edda.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMobileService service;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMobileService _service)
         {
             _logger = logger;
+            service = _service;
         }
 
         public IActionResult Index()
         {
+            var mobiles = service.GetAllAsync().Result;
+            ViewBag.TopSellers = mobiles.OrderByDescending(p => p.UnitsSold).Take(3);
+            ViewBag.NewArrivals = mobiles.OrderByDescending(p => p.ReleaseDate).Take(3);
+            ViewBag.HotSales = mobiles.OrderBy(p => p.Price).Take(3);
             return View();
         }
 
